b9a240a618ed4bb6bc807d3cc868fdf87b735ce4
Phillip Kim <philkim78@gmail.com>
2019-12-28 09:30:39 +1100

    Added some debug lines to understand what is happening.

--- ratings-counter.py
+++ ratings-counter.py
@@ -1,18 +1,51 @@
 from pyspark.sql import SparkSession
-from pyspark.conf import SparkConf
 import collections
 
-conf = SparkConf().setAppName("RatingsHistogram")
-spark = SparkSession.builder.config(conf=conf).getOrCreate()
-
+# Use existing Databricks Connect session
+# Note: Cannot modify existing SparkConf
+spark = SparkSession.builder.getOrCreate()
 sc = spark.sparkContext
 # sc.setLogLevel("INFO")
 
 lines = sc.textFile("s3a://mypersonaldumpingground/ml-100k/u.data")
-
+print(type(lines))
 ratings = lines.map(lambda x: x.split()[2])
+print(type(ratings))
+print(ratings.take(5))
+
+'''
+User Id, Movie ID, Rating, Timestamp
+196	242	3	881250949
+186	302	3	891717742
+22	377	1	878887116
+244	51	2	880606923
+166	346	1	886397596
+'''
+
 result = ratings.countByValue()
+# result is collections.defaultdict
+print(type(result))
+print(sorted(result.items()))
 
 sortedResults = collections.OrderedDict(sorted(result.items()))
+print(type(sortedResults))
+print(sortedResults)
+
 for key, value in sortedResults.items():
     print("%s %i" % (key, value))
+
+
+### Original Example ###
+# from pyspark import SparkConf, SparkContext
+# import collections
+#
+# conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
+# sc = SparkContext(conf = conf)
+#
+# lines = sc.textFile("file:///SparkCourse/ml-100k/u.data")
+# ratings = lines.map(lambda x: x.split()[2])
+# result = ratings.countByValue()
+#
+# sortedResults = collections.OrderedDict(sorted(result.items()))
+# for key, value in sortedResults.items():
+#     print("%s %i" % (key, value))
