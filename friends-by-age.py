cb5274b8b22fe9788455e0dda68aae4a19776908
Phillip Kim <philkim78@gmail.com>
2019-12-30 13:20:22 +1100

    Modified Key Value RDD example and added filtering RDD example file.

--- friends-by-age.py
+++ friends-by-age.py
@@ -15,14 +15,30 @@ def parseLine(line):
 
 
 lines = sc.textFile("s3a://mypersonaldumpingground/spark_taming_data/fakefriends.csv")
+
+# Input data
+'''
+0,Will,33,385
+1,Jean-Luc,26,2
+2,Hugh,55,221
+3,Deanna,40,465
+4,Quark,68,21
+'''
+
 rdd = lines.map(parseLine)
+print(rdd.take(5))
+
 totalsByAge = rdd.mapValues(lambda x: (x, 1)).reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1]))
-averagesByAge = totalsByAge.mapValues(lambda x: x[0] / x[1])
+print(totalsByAge.take(5))
+
+averagesByAge = totalsByAge.mapValues(lambda x: int(x[0] / x[1]))
+print(averagesByAge.take(5))
+
 results = averagesByAge.collect()
+print(type(results))
 
 for result in results:
     print(result)
-
 
 
 ### Original Example ###
