98a7c28eaa96cd65fa9af4ee8a2403f58c6e153c
Phillip Kim <philkim78@gmail.com>
2020-01-04 20:15:23 +1100

    Accumulators not working for Databricks Connect.

--- accumulator_play.py
+++ accumulator_play.py
@@ -1,11 +1,26 @@
-from pyspark import SparkContext
-sc = SparkContext("local", "Accumulator app")
-sc.setLogLevel("WARN")
-num = sc.accumulator(0)
+from pyspark.sql import SparkSession
 
-def f(x):
-    # global num
-    num.add(x)
+# Use existing Databricks Connect session
+# Note: Cannot modify existing SparkConf
+spark = SparkSession.builder.getOrCreate()
+sc = spark.sparkContext
+# sc.setLogLevel("INFO")
 
-rdd = sc.parallelize([1, 2, 3, 4]).foreach(f)
-print("Accumulated value is {}".format(num.value))
+# from pyspark import SparkContext
+# sc = SparkContext("local", "Accumulator app")
+# sc.setLogLevel("WARN")
+print(type(spark))
+print(type(sc))
+print(help(sc))
+
+
+# num = sc.accumulator(0)
+#
+#
+# def f(x):
+#     # global num
+#     num.add(x)
+#
+#
+# rdd = sc.parallelize([1, 2, 3, 4]).foreach(f)
+# print("Accumulated value is {}".format(num.value))
